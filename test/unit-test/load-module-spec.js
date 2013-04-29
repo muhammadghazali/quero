c29b03609284875e667b5fd073da7f585db495d5
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-04-30 05:43:07 +0700

    Setup app skeleton

---
+++ test/unit-test/load-module-spec.js
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var
+  vows = require('vows'),
+  assert = require('assert');
+
+vows.describe('Should be loaded')
+.addBatch({
+  'Load module': {
+    topic: function () {
+      return quero = require('./../../');
+    },
+      'should be loaded': function (topic) {
+      assert.isObject(topic);
+      assert.isNotNull(topic);
+    }
+  }
+})
+.export(module);
