0c9a42e6d7d348ff34b3adec27fc2c6fb468335e
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-01 16:26:00 +0700

    Handling the query that produce output in JSON, which is stumbleupon and facebook

--- test/unit-test/ping-spec.js
+++ test/unit-test/ping-spec.js
@@ -44,52 +44,7 @@ vows.describe('Should handle URLs argument properly')
         quero.ping(urls[0], this.callback);
       },
       'should return a result if succeed': function (err, result) {
-        assert.isNull(err);
-        assert.isObject(result);
-      }
-    },
-    'Handle one more than URLs': {
-      topic: function (quero) {
-        quero.ping(urls, this.callback);
-      },
-      'should return a result if succeed': function (err, result) {
-        assert.isNull(err);
-        assert.isObject(result);
-      }
-    },
-    'Handle an empty URLs': {
-      topic: function (quero) {
-        quero.ping([], this.callback);
-      },
-      'should return a result if succeed': function (err, result) {
-        assert.throws(err, Error);
-        assert.isUndefined(result);
-      }
-    }
-  }
-})
-  .export(module);
-
-
-vows.describe('Should query the APIs')
-  .addBatch({
-  'Load module': {
-    topic: function () {
-      quero = require('./../../');
-      return quero;
-    },
-    'should be loaded': function (topic) {
-      assert.isObject(topic);
-      assert.isNotNull(topic);
-    },
-    'should have a ping function': function (topic) {
-      assert.isFunction(topic.ping);
-    },
-    'Query one URL': {
-      topic: function (quero) {
-        quero.ping(urls[0], this.callback);
-      },
-      'should return a result if succeed': function (err, result) {
+        console.log('result', result);
         assert.isNull(err);
         assert.isObject(result);
         assert.isTrue(checkQueryResults(result));
