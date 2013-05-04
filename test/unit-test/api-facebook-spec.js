b8e54bc37f2605c94790eb084bc4feb074ec3da0
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-05 00:27:34 +0700

    Write a test suite for each of api modules

---
+++ test/unit-test/api-facebook-spec.js
@@ -0,0 +1,24 @@
+/**
+ * Facebook API test suite
+ */
+
+var
+  vows = require('vows'),
+  assert = require('assert');
+
+vows.describe('Should load the module')
+  .addBatch({
+  'Load the module': {
+    topic: function () {
+      return require('./../../lib/apis/facebook');
+    },
+    'should load the module': function (topic) {
+      assert.isObject(topic);
+      assert.include(topic, 'getUrl');
+      assert.include(topic, 'query');
+      assert.isFunction(topic.getUrl);
+      assert.isFunction(topic.query);
+    }
+  }
+})
+  .export(module);
---
+++ test/unit-test/api-pinterest-spec.js
@@ -0,0 +1,24 @@
+/**
+ * Pinterest API test suite
+ */
+
+var
+  vows = require('vows'),
+  assert = require('assert');
+
+vows.describe('Should load the module')
+  .addBatch({
+  'Load the module': {
+    topic: function () {
+      return require('./../../lib/apis/pinterest');
+    },
+    'should load the module': function (topic) {
+      assert.isObject(topic);
+      assert.include(topic, 'getUrl');
+      assert.include(topic, 'query');
+      assert.isFunction(topic.getUrl);
+      assert.isFunction(topic.query);
+    }
+  }
+})
+  .export(module);
