ec054c567cca9ab5c2d1f19f4f5e36638b89530b
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-06 00:53:41 +0700

    Fix linkedin api request module

--- test/unit-test/api-linkedin-spec.js
+++ test/unit-test/api-linkedin-spec.js
@@ -6,7 +6,7 @@ var
   vows = require('vows'),
   assert = require('assert');
 
-var linkedinApi = require('./../../lib/apis').facebook;
+var linkedinApi = require('./../../lib/apis').linkedin;
 
 vows.describe('Should load the module')
   .addBatch({
@@ -14,23 +14,24 @@ vows.describe('Should load the module')
     topic: linkedinApi,
     'should load the module': function (topic) {
       assert.isObject(topic);
-      assert.include(topic, 'getUrl');
       assert.include(topic, 'query');
-      assert.isFunction(topic.getUrl);
       assert.isFunction(topic.query);
     }
   }
 })
   .export(module);
 
-vows.describe('Should replace the url holder with encoded url')
+vows.describe('Query the linkedin api should return total count')
   .addBatch({
   'Should build the request url': {
     topic: function () {
-      return linkedinApi.getUrl('http://github.com');
+      linkedinApi.query('http://github.com', this.callback);
     },
-    'should build the request url': function (topic) {
-      assert.isString(topic);
+    'should return the total count': function (err, result) {
+      assert.isNull(err);
+      assert.isObject(result);
+      assert.include(result, 'count');
+      assert.isNumber(result.count);
     }
   }
 })
