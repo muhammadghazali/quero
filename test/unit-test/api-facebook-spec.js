6aaf03be310923e510d266d960ded0ab24bb21f4
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-06 00:25:20 +0700

    Refactor function name and fix test suite

--- test/unit-test/api-facebook-spec.js
+++ test/unit-test/api-facebook-spec.js
@@ -14,23 +14,24 @@ vows.describe('Should load the module')
     topic: facebookApi,
     'should load the module': function (topic) {
       assert.isObject(topic);
-      assert.include(topic, 'getUrl');
       assert.include(topic, 'query');
-      assert.isFunction(topic.getUrl);
       assert.isFunction(topic.query);
     }
   }
 })
   .export(module);
 
-vows.describe('Should replace the url holder with encoded url')
+vows.describe('Query the facebook api should return total count')
   .addBatch({
   'Should build the request url': {
     topic: function () {
-      return facebookApi.getUrl('http://github.com');
+      facebookApi.query('http://github.com', this.callback);
     },
-    'should build the request url': function (topic) {
-      assert.isString(topic);
+    'should return the total count': function (err, result) {
+      assert.isNull(err);
+      assert.isObject(result);
+      assert.include(result, 'count');
+      assert.isNumber(result.count);
     }
   }
 })
