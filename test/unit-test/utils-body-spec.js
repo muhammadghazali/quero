912d200735001778afa88522cf783d7805a6e595
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-17 01:02:38 +0700

    Hardening the internal body utils module

--- test/unit-test/utils-body-spec.js
+++ test/unit-test/utils-body-spec.js
@@ -6,28 +6,13 @@ var
   vows = require('vows'),
   assert = require('assert');
 
-vows.describe('Should load the module')
-  .addBatch({
-  'Load the module': {
-    topic: function () {
-      return require('./../../lib/utils/');
-    },
-    'should load the module': function (topic) {
-      assert.include(topic, 'body');
-      assert.include(topic, 'url');
-    }
-  }
-})
-  .export(module);
+var bodyUtils = require('./../../lib/utils/').body;
 
 vows.describe('Should load the module')
   .addBatch({
   'Load the module': {
-    topic: function () {
-      return require('./../../lib/utils/body');
-    },
+    topic: bodyUtils,
     'should load the module': function (topic) {
-      assert.isObject(topic);
       assert.include(topic, 'parse');
       assert.isFunction(topic.parse);
     }
@@ -35,14 +20,57 @@ vows.describe('Should load the module')
 })
   .export(module);
 
-vows.describe('Should be able to parse application/javascript;charset=utf-8 body')
+vows.describe('Parse response body')
   .addBatch({
-  'Load the module': {
+  'Handle application/javascript;charset=utf-8 content type': {
     topic: function () {
-      var body = require('./../../lib/utils/body');
 
-      return body.parse('application/javascript;charset=utf-8',
+      return bodyUtils.parse('application/javascript;charset=utf-8',
         'twttr.receiveCount/({"count":7020,"url":"http:\\/\\/google.com\\/"});');
+    },
+    'should parse the application/javascript body successfully':
+      function (topic) {
+        assert.isObject(topic);
+      }
+  },
+  'Handle application/javascript;charset=UTF-8 content type': {
+    topic: function () {
+
+      return bodyUtils.parse('application/javascript;charset=utf-8',
+        '{"count":0,"fCnt":"0","fCntPlusOne":"1","url":"https:\/\/www.odesk.com\/home"}');
+    },
+    'should parse the application/javascript body successfully':
+      function (topic) {
+        assert.isObject(topic);
+      }
+  },
+  'Handle application/javascript content type': {
+    topic: function () {
+
+      return bodyUtils.parse('application/javascript',
+        'receiveCount({"count": 0, "url": "https://www.odesk.com/home"})');
+    },
+    'should parse the application/javascript body successfully':
+      function (topic) {
+        assert.isObject(topic);
+      }
+  },
+  'Handle application/json content type': {
+    topic: function () {
+
+      return bodyUtils.parse('application/json',
+        '{"url":"https:\/\/www.odesk.com\/home","normalized_url":"https:\/\/www.odesk.com\/home","share_count":9,"like_count":11,"comment_count":0,"total_count":20,"click_count":0,"comments_fbid":10150158925778482,"commentsbox_count":0}');
+    },
+    'should parse the application/javascript body successfully':
+      function (topic) {
+        assert.isObject(topic);
+      }
+  },
+  'Handle content type other than JSON': {
+    topic: function () {
+
+      return bodyUtils.parse('text/plain; charset=iso-8859-1',
+        '{"result":{"url":"https:\/\/www.odesk.com\/home","in_index":true,"publicid":"2LC2PN","views":4,"title":"            Sign In - oDesk           ","thumbnail":"http:\/\/cdn.stumble-upon.com\/images\/nomthumb.png","thumbnail_b":"http:\/\/cdn.stumble-upon.com\/images\/nobthumb.png","submit_link":"http:\/\/www.stumbleupon.com\/submit\/?url=https:\/\/www.odesk.com\/home","badge_link":"http:\/\/www.stumbleupon.com\/badge\/?url=https:\/\/www.odesk.com\/home","info_link":"http:\/\/www.stumbleupon.com\/url\/https%253A\/\/www.odesk.com\/home"},"timestamp":1367185449,"success":true}');
     },
     'should parse the application/javascript body successfully':
       function (topic) {
@@ -52,47 +80,11 @@ vows.describe('Should be able to parse application/javascript;charset=utf-8 body
 })
   .export(module);
 
-vows.describe('Should be able to parse application/javascript body')
+vows.describe('Pass invalid arguments')
   .addBatch({
-  'Load the module': {
+  'Should throw Error if content-type argument is not string': {
     topic: function () {
-      var body = require('./../../lib/utils/body');
-
-      return body.parse('application/javascript',
-        'twttr.receiveCount/({"count":7020,"url":"http:\\/\\/google.com\\/"});');
-    },
-    'should parse the application/javascript body successfully':
-      function (topic) {
-        assert.isObject(topic);
-      }
-  }
-})
-  .export(module);
-
-vows.describe('Should be able to parse application/json body')
-  .addBatch({
-  'Load the module': {
-    topic: function () {
-      var body = require('./../../lib/utils/body');
-
-      return body.parse('application/json;charset=utf-8',
-        '{"result":{"url":"https:\/\/www.odesk.com\/home","in_index":true,"publicid":"2LC2PN","views":4,"title":"Sign In - oDesk           ","thumbnail":"http:\/\/cdn.stumble-upon.com\/images\/nomthumb.png","thumbnail_b":"http:\/\/cdn.stumble-upon.com\/images\/nobthumb.png","submit_link":"http:\/\/www.stumbleupon.com\/submit\/?url=https:\/\/www.odesk.com\/home","badge_link":"http:\/\/www.stumbleupon.com\/badge\/?url=https:\/\/www.odesk.com\/home","info_link":"http:\/\/www.stumbleupon.com\/url\/https%253A\/\/www.odesk.com\/home"},"timestamp":1367185449,"success":true}');
-    },
-    'should parse the application/json body successfully':
-      function (topic) {
-        assert.isObject(topic);
-      }
-  }
-})
-  .export(module);
-
-vows.describe('Should throw Error if content-type is not string')
-  .addBatch({
-  'Load the module': {
-    topic: function () {
-      var body = require('./../../lib/utils/body');
-
-      return body.parse(1,
+      return bodyUtils.parse(1,
         '{"result":{"url":"https:\/\/www.odesk.com\/home","in_index":true,"publicid":"2LC2PN","views":4,"title":"Sign In - oDesk           ","thumbnail":"http:\/\/cdn.stumble-upon.com\/images\/nomthumb.png","thumbnail_b":"http:\/\/cdn.stumble-upon.com\/images\/nobthumb.png","submit_link":"http:\/\/www.stumbleupon.com\/submit\/?url=https:\/\/www.odesk.com\/home","badge_link":"http:\/\/www.stumbleupon.com\/badge\/?url=https:\/\/www.odesk.com\/home","info_link":"http:\/\/www.stumbleupon.com\/url\/https%253A\/\/www.odesk.com\/home"},"timestamp":1367185449,"success":true}');
     },
     'should throw Error':
@@ -100,17 +92,20 @@ vows.describe('Should throw Error if content-type is not string')
         assert.throws(topic, Error);
         assert.isObject(topic);
       }
-  }
-})
-  .export(module);
-
-vows.describe('Should throw Error if body is not string')
-  .addBatch({
-  'Load the module': {
+  },
+  'Should throw Error if body argument is not string': {
     topic: function () {
-      var body = require('./../../lib/utils/body');
-
-      return body.parse('application/json;charset=utf-8', 1);
+      return bodyUtils.parse('application/json;charset=utf-8', 1);
+    },
+    'should throw Error':
+      function (topic) {
+        assert.throws(topic, Error);
+        assert.isObject(topic);
+      }
+  },
+  'Should throw Error if arguments is not string': {
+    topic: function () {
+      return bodyUtils.parse(1, 1);
     },
     'should throw Error':
       function (topic) {
