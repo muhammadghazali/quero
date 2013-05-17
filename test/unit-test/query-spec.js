9f291059a0c82260c1894685c0ec5e6efae02c75
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-17 19:44:34 +0700

    Achieve more code coverage on query

--- test/unit-test/query-spec.js
+++ test/unit-test/query-spec.js
@@ -2,18 +2,38 @@ var
   vows = require('vows'),
   assert = require('assert');
 
-var apis = {};
+var apis = require('./../../lib/apis');
 
-vows.describe('Should be loaded')
+var validUrls = [
+  {identifier: 'nodejs homepage', url: 'http://nodejs.org'},
+  {identifier: 'npm homepage', url: 'http://npmjs.org'},
+  {identifier: 'yahoo homepage', url: 'http://www.yahoo.com'},
+  {identifier: 'google homepage', url: 'http://google.com'}
+];
+
+vows.describe('Should load the module')
   .addBatch({
   'Load module': {
-    topic: function () {
-      return apis = require('./../../lib/apis');
-    },
-    'should be loaded': function (topic) {
-      assert.isObject(topic);
-      assert.isNotNull(topic);
+    topic: apis,
+    'should load the module': function (topic) {
+      assert.include(topic, 'query');
       assert.isFunction(topic.query);
+    }
+  }
+})
+  .export(module);
+
+vows.describe('Should be able to ping one url')
+  .addBatch({
+  'Query valid urls': {
+    topic: function () {
+      apis.query(validUrls, this.callback);
+    },
+    'should return a result if succeed': function (err, result) {
+      console.log('result', result);
+      assert.isNull(err);
+      assert.isArray(result);
+      assert.isObject(result[0]);
     }
   }
 })
